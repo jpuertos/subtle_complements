9c1962d9235091c00deb77dd0b0f21a814250eca
Juan V. Puertos <juanvi.puertos@gmail.com>
2012-01-26 03:11:42 +0000

    Added both files. Works like a charm. Thanks unexist.

---
+++ nettraffic/nettraffic.rb
@@ -0,0 +1,31 @@
+# Nettraffic sublet file
+# Created with sur-0.2
+helper do
+  def read_byte_counters(iface_name)
+    rx_bytes = 0
+    tx_bytes = 0
+    res = IO.readlines("/proc/net/dev")
+    res.each do |l|
+      if l.scan(iface_name).size == 1
+        rx_bytes = l.split[1].to_i
+        tx_bytes = l.split[9].to_i
+        break
+      end
+    end
+    return rx_bytes, tx_bytes
+  end
+end
+
+configure :nettraffic do |s|
+  s.interval = 1
+  s.iface_name = "ppp0" #User MUST specify this, how?
+  s.last_counters = read_byte_counters(s.iface_name)
+end
+
+on :run do |s|
+  current_counters = read_byte_counters(s.iface_name)
+  rx_speed = (current_counters[0] - s.last_counters[0])/1000
+  tx_speed = (current_counters[1] - s.last_counters[1])/1000
+  s.last_counters = current_counters
+  s.data = "D:#{rx_speed} KB/s U:#{tx_speed} KB/s"
+end
